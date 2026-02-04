8ecfc3650a420d4848a3ae9201edf5dd68e6723f
samraina <172354663+samSRaina@users.noreply.github.com>
2026-02-04 14:04:08 +0530

    Implemented access via SMILE, new API + JS updated
    Implemented new feature to also access data via the SMILE values and the
    additional API to do the same. Added a toggle to switch between the 2 search modes.
    Updated the JS to handle the toggle switch.
    Minor changes to the .css file.

--- src/services/logic.py
+++ src/services/logic.py
@@ -1,6 +1,6 @@
 import requests, logging
 from functools import lru_cache
-import json
+from fastapi import HTTPException
 
 logging.basicConfig(level=logging.INFO)
 logger= logging.getLogger(__name__)
@@ -22,7 +22,6 @@ def get_smile(name: str) -> str | None:
             logger.error(f"{e}")
         return None
 
-
 properties = [
     "SMILES",
     "IUPACName",
@@ -35,9 +34,29 @@ properties = [
     "HBondAcceptorCount",
     "RotatableBondCount",
 ]
+props_string=",".join(properties)
+
+
+#input via compound name
 def get_properties(name: str):
-    props_string=",".join(properties)
     url=f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{name}/property/{props_string}/JSON"
+
+    try:
+        r = requests.get(url, timeout=10)
+        r.raise_for_status()
+
+        data=r.json()
+        return data["PropertyTable"]["Properties"][0]
+    except requests.RequestException as e:
+        if e.response:
+            logger.error(f"{e.response.status_code}")
+        else:
+            logger.error(f"{e}")
+        return None
+
+#input via smile
+def get_properties_via_smile(smile: str):
+    url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/smiles/{smile}/property/{props_string}/JSON"
 
     try:
         r = requests.get(url, timeout=10)
